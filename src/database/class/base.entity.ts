a8b08b1ebb6aa9a452a51f2d7eb28ff3a23f9f0f
Taufik Rahadi <taufik.rahadi@akarinti.tech>
2023-05-09 15:18:53 +0700

    chore(base entity): add class base entity that contains id, timestamp, and deleted at columns

---
+++ src/database/class/base.entity.ts
@@ -0,0 +1,20 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id?: number;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt?: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamptz', nullable: true })
+  deletedAt?: Date;
+}
