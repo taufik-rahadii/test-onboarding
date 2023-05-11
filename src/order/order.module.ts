58a278c50ceb521f865cf920a7f49886f28f89cb
Taufik Rahadi <taufik.rahadi@akarinti.tech>
2023-05-11 07:25:11 +0700

    chore: inject pagination service inside order module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,10 +6,11 @@ import { ResponseService } from 'src/response/response.service';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { ProductModule } from 'src/product/product.module';
+import { PaginationService } from 'src/utils/pagination.service';
 
 @Module({
   imports: [ProductModule, TypeOrmModule.forFeature([Order, OrderDetail])],
-  providers: [ResponseService, OrderService],
+  providers: [ResponseService, OrderService, PaginationService],
   controllers: [OrderController],
 })
 export class OrderModule {}
